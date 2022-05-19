df9b5db13043c99eb3e68e35419dc9c3f4ece053
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2022-05-19 17:37:01 -0400

    Update BooleanSource.java

--- BooleanSource.java
+++ BooleanSource.java
@@ -1,7 +1,7 @@
 
 public class BooleanSource {
 	private double probability;
-	
+	private double testing;
 	public BooleanSource(double probability) throws IllegalArgumentException{
 		if(probability<0 || probability >1) {
 			throw new IllegalArgumentException();
